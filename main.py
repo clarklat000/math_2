0545d5dbab255e9899e77565cc9a1227ed94ad55
Latrell Clark <clarklat000@warren.k12.in.us>
2024-08-23 08:25:31 -0400

    math2

---
+++ main.py
@@ -0,0 +1,7 @@
+miles = 5
+minutes = 45
+hours = minutes / 60
+mph = miles / hours
+print("how many miles did you run?", miles)
+print ("how many minutes id it take you", minutes)
+print('speed in mph:', mph)
